0f19363feab1d624fd07ba464e00a4cb78bef893
thais.souza <thaisouzacruz@gmail.com>
2022-10-19 16:43:55 -0300

    ajuste imagem

--- src/app/produtos.ts
+++ src/app/produtos.ts
@@ -19,7 +19,7 @@ export const produtos: IProduto[] = [
   { id: 5, descricao: "Fone de ouvido", preco: 299.99, descricaoPreco: "À vista no PIX", imagem: "./assets/fone-de-ouvido-1.jpg",quantidadeEstoque: 7 },
   { id: 6, descricao: "Fone de ouvido bom", preco: 399.99, descricaoPreco: "À vista no PIX", imagem: "./assets/fone-de-ouvido-3.jpg",quantidadeEstoque: 20 },
   { id: 7, descricao: "HD 1TB", preco: 499.99, descricaoPreco: "À vista no PIX", imagem: "./assets/hd.jpg",quantidadeEstoque: 20 },
-  { id: 8, descricao: "Combo de placa de vídeos", preco: 18449.99, descricaoPreco: "À vista no PIX", imagem: "/assets/placa-video.jpg",quantidadeEstoque: 17 },
+  { id: 8, descricao: "Combo de placa de vídeos", preco: 18449.99, descricaoPreco: "À vista no PIX", imagem: "./assets/placa-video.jpg",quantidadeEstoque: 17 },
   { id: 9, descricao: "Processador Ryzen", preco: 1000, descricaoPreco: "À vista no PIX", imagem: "./assets/processador.jpg",quantidadeEstoque: 32 },
   { id: 10, descricao: "Notebook bom", preco: 2500, descricaoPreco: "À vista no PIX", imagem: "./assets/laptop-1.jpg",quantidadeEstoque: 10 },
   { id: 11, descricao: "Notebook excelente", preco: 4500, descricaoPreco: "À vista no PIX", imagem: "./assets/laptop-2.jpg",quantidadeEstoque: 22 },
@@ -27,4 +27,5 @@ export const produtos: IProduto[] = [
   { id: 13, descricao: "Mouse ótimo", preco: 200, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-2.jpg",quantidadeEstoque: 17 },
   { id: 14, descricao: "Mouse pequeno", preco: 50, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-4.jpg",quantidadeEstoque: 10 },
   { id: 15, descricao: "Teclado bom", preco: 159.99, descricaoPreco: "À vista no PIX", imagem: "./assets/teclado-2.jpg",quantidadeEstoque: 18 },
+  { id: 16, descricao: "Teclado bom", preco: 159.99, descricaoPreco: "À vista no PIX", imagem: "./assets/teclado-2.jpg",quantidadeEstoque: 20 }
 ]
